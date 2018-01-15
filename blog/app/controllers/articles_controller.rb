7beb9b6195124cec87cb1126a3b64c55c5173ba9
Manuel Blanco Rienda <manuelbr@MacBook-Pro-de-Manuel.local>
2018-01-15 17:56:13 +0100

    incluido el contenido de la mitad del capítulo 9

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,10 +1,20 @@
 class ArticlesController < ApplicationController
   # Funcionalidad index del controlador articles.
+  # GET articles
+  # Muestra todos los artículos
   def index
     @article = Article.all
   end
 
+  # GET articles/:id
+  #Muestra un artículo en concreto
   def show
     @article = Article.find(params[:id])
+  end
+
+  #GET articles/new
+  #Aquí se define la lógica que hay que seguir para introducir una instancia de artículo en base de datos.
+  def new
+    @article = Article.new
   end
 end
