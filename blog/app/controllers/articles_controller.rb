6a593982bc3bb4078dda07861c835a5e2b2f6498
Manuel Blanco Rienda <manuelbr@MacBook-Pro-de-Manuel.local>
2018-01-16 17:38:04 +0100

    Subida de los ficheros fuente del proyecto hasta el capítulo 9 incluido

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -13,8 +13,18 @@ class ArticlesController < ApplicationController
   end
 
   #GET articles/new
-  #Aquí se define la lógica que hay que seguir para introducir una instancia de artículo en base de datos.
+  #Aquí se define la lógica que hay que seguir para introducir una instancia vacía de artículo en base de datos.
   def new
+    #Como no se guarda después de haberlo creado, no entra en la base de datos. Es como un constructor por defecto.
     @article = Article.new
+  end
+
+  #POST articles/create
+  #Aquí se define la lógica que hay que seguir para crear la nueva instancia que se ha definido en new
+  def create
+    @article = Article.new(titulo: params[:article][:titulo], contenido: params[:article][:titulo])
+    @article.save
+
+    redirect_to @article
   end
 end
